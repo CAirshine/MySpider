b4c56167d5a72446b492f1bbcb4d7a5730b95e14
FLY <FLY@192.168.0.106>
2017-12-03 14:10:51 +0800

    Change-Id: I102e863ec08ee1a4bbb18d29f4033160172871b0 Signed-off-by: FLY <FLY@192.168.0.106>

--- src/myspider/MyPageProcessor.java
+++ src/myspider/MyPageProcessor.java
@@ -1,5 +1,8 @@
 package myspider;
 
+import java.io.BufferedWriter;
+import java.util.TreeMap;
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -7,14 +10,36 @@ import us.codecraft.webmagic.processor.PageProcessor;
 
 public class MyPageProcessor implements PageProcessor {
 
-	private Site site = Site.me().setRetryTimes(30).setSleepTime(100);
+	private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
 
+	private TreeMap<String, String> map = new TreeMap<String, String>();
+	private String url = "";
+	private long num = 0L;
+	
+	private BufferedWriter Writer;
+	
 	@Override
 	public void process(Page page) {
-		
-		page.addTargetRequests(page.getHtml().links().regex("(https://www.qiushibaike.com/\\w+)").all());
-		
-		page.putField("content", page.getHtml().xpath("//div[@class=\"content\"/span/]"));
+
+		url = page.getUrl().toString();
+		if (map.get(url) == null) {
+			
+			map.put(url, "");
+			
+			page.putField("title", page.getHtml().xpath("//title/text()").toString());
+			page.putField("content", page.getHtml().xpath("//div[@class=\"content-text\"]/text()").toString());
+
+			if (page.getResultItems().get("content") == null) {
+				System.out.println("[SKIP]	" + page.getUrl());
+				page.setSkip(true);
+			}
+			
+			System.out.println("------------------------------------" + num++ + "------------------------------------");
+		} else {
+			System.out.println("[INFO]	Ìø¹ýÖØ¸´");
+		}
+
+		page.addTargetRequests(page.getHtml().links().regex("(https://www.qiushibaike.com/article/\\w+)").all());
 	}
 
 	@Override
@@ -23,7 +48,9 @@ public class MyPageProcessor implements PageProcessor {
 	}
 
 	public static void main(String[] args) {
-		
-		Spider.create(new MyPageProcessor()).addUrl("https://www.qiushibaike.com/hot/").thread(5).run();
+
+		// while (true) {
+		Spider.create(new MyPageProcessor()).addUrl("https://www.qiushibaike.com/article/118981526").thread(1).run();
+		// }
 	}
 }
